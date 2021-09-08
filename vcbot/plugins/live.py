83c6880846d57a11b04cb4f6ea721811d9e9929f
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-08 23:15:11 +0700

    fixes
    fixed live streaming issues

--- vcbot/plugins/live.py
+++ vcbot/plugins/live.py
@@ -1,7 +1,5 @@
-import logging
 import re
 import os
-import signal
 import asyncio
 import traceback
 from asyncio import sleep
@@ -37,13 +35,21 @@ async def stream_msg_handler(_, m: Message):
             if (await is_ytlive(stream_url)):
                 stream_url = await convert_to_stream(link)
             else:
-                player.meta["is_live"] = False
+                # player.meta["is_live"] = False
                 stream_url, _ = await yt_download(link)
                 player.add_to_trash(stream_url)
     except IndexError:
         ...
-    audio, video = await player.convert(stream_url, daemon=True, delete=False)
+    audio = f"audio{m.chat.id}.raw"
+    video = f"video{m.chat.id}.raw"
+    audio, video = await player.convert(stream_url,
+                                        daemon=True,
+                                        delete=False,
+                                        audio_file=audio,
+                                        video_file=video)
     player.meta["is_playing"] = True
+    while not os.path.exists(audio) and not os.path.exists(video):
+        await asyncio.sleep(0.5)
     await group_calls.join_group_call(
         m.chat.id,
         InputAudioStream(
